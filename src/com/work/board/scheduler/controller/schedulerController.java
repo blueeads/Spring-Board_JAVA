a3b1334f566a358ecacd7716d584071602ae4249
Ohyeon Kwon <37339357+blueeads@users.noreply.github.com>
2019-09-25 14:44:13 +0900

    Update schedulerController.java
    파일을 다시 Import하는 과정에 한글이 깨져 다시 작성

--- src/com/work/board/scheduler/controller/schedulerController.java
+++ src/com/work/board/scheduler/controller/schedulerController.java
@@ -7,18 +7,18 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class schedulerController {
-	// * �쓣 �엯�젰�븷寃쎌슦 紐⑤몢(�빆�긽)�쑝濡� �꽕�젙�븿.
-	//                 珥�  遺�  �떆  �씪  �썡  �슂�씪
+	// 오후 1시에 스케줄 컨트롤러 작동
+	// 파일 업로드를 DB에 바로 저장하기 때문에 지금은 사용 X
 	@Scheduled(cron = "0 00 13 * * *")
 	public void autoUpdate(){
 		String path = "C:/Temp/TsetFolder/";
 		File folder = new File(path);
 		try {
 		    while(folder.exists()) {
-			File[] folder_list = folder.listFiles(); //�뙆�씪由ъ뒪�듃 �뼸�뼱�삤湲�
+			File[] folder_list = folder.listFiles(); //폴더 리스트 파일 개수 확인
 					
 			for (int j = 0; j < folder_list.length; j++) {
-				folder_list[j].delete(); //�뙆�씪 �궘�젣 
+				folder_list[j].delete(); //내부 폴더 모두 삭제
 				System.out.println("파일 삭제 ");
 						
 			}
